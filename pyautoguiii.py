c5614e301c43104f1d245cdef23f2eb760a24b04
miladbarouni <132874873+miladbarouni@users.noreply.github.com>
2023-09-09 06:33:36 +0330

    Add files via upload

--- pyautoguiii.py
+++ pyautoguiii.py
@@ -2,9 +2,10 @@
 import random
 import pyautogui as py
 
-all_bot_name = ['Milad', 'Misaq', 'Aida', 'Neda']
+
+all_bot_name = ['Milad', 'Misaq', 'Abas', 'Mohammad', 'Aida', 'Neda', 'Sepideh', 'Mahsa']
 action = ['Rock', 'Paper', 'Scissor']
-user = 0
+user_1 = 0
 user_2 = 0
 bot_1 = 0
 bot_2 = 0
@@ -12,47 +13,64 @@ bot_2 = 0
 welcome_panel = py.confirm(text='Welcome to the game, Are you ready to start ?', title='Welcome panel', buttons=['Yes', 'No'])
 
 while welcome_panel == 'No' :
-    canceling = py.confirm(text='Are you sure to quit the game ?', title='quit panel', buttons=['Yes', 'No'])
+    canceling = py.confirm(text='Are you sure to quit the game ?', title='Quit panel', buttons=['Yes', 'No'])
     if canceling == 'Yes' :
-        canceling_2 = py.alert(text='Bye, we will see you soon again', title='quit panel')
+        canceling_2 = py.alert(text='Bye, we hope see you soon again', title='Quit panel')
         break
     elif canceling == 'No' :
         welcome_panel = py.confirm(text='Welcome to the game, Are you ready to start ?', title='Welcome panel', buttons=['Yes', 'No'])
 
 if welcome_panel == 'Yes' :
-    username = py.prompt(text='Game is started, first enter your name please', title='Name panel', default='Your name')
-    while username == '' :                                                                                                    #????????
-        canceling = py.confirm(text='Are you sure to quit the game ?', title='Welcome panel', buttons=['Yes', 'No'])
+    username = py.prompt(text='First enter your name please', title='Name panel', default='Your name')
+    while not username :                                                                                                    #????????
+        canceling = py.confirm(text='Are you sure to quit the game ?', title='Quit panel', buttons=['Yes', 'No'])
         if canceling == 'Yes' :
-            canceling_2 = py.alert(text='Wish see you soon, Thanks for selecting us', title='Welcome panel')
+            canceling_2 = py.alert(text='Bye, we hope see you soon again', title='Quit panel')
             break
-        while canceling == 'No' :
-            username = py.prompt(text='Game is started, first enter your name please', title='Name panel', default='Your name')
+        if canceling == 'No' :
+            username = py.prompt(text='First enter your name please', title='Name panel', default='Your name')
     else :
-        which_game = py.confirm(text=f'{username} which game do you want ?', title='select game panel', buttons=['User - Bot', 'Bot - Bot', 'User - User', 'Rull game', 'Details'])
-        round_number = int(py.prompt(text='How many round are played ?', title='round panel', default='How round'))
+        which_game = py.confirm(text=f'{username} which game do you want ?', title='select game panel', buttons=['User - Bot', 'Bot - Bot', 'User - User', 'Rull game', 'Details', 'Exit'])
 
-random_bot_name = random.choice(all_bot_name)
+while which_game == 'Rull game' or 'Details' or 'Exit' :
+    if which_game == 'Rull game' :
+        Rull_game = py.alert(text='Rull of the game: 1. Playing the game means you have accepted the complete rules of the game. 2. When your opponent is a robot, his movement is chosen by chance and you have to accept the result. If you make a change in the source code of the game, this is cheating. 3. During the game, two users should not show each other their next move to the opponent. 4. If you withdraw from the game, your opponent will win the game.', title='Rull panel')
+        which_game = py.confirm(text=f'{username} which game do you want ?', title='select game panel', buttons=['User - Bot', 'Bot - Bot', 'User - User', 'Rull game', 'Details', 'Exit'])
+    elif which_game == 'Details' :
+        Details_game = py.alert(text='This game produced by Milad Barouni and the Ways of communication by Email : milad.baroun@gmail.com and Phone number : +989177719601', title='Details panel')
+        which_game = py.confirm(text=f'{username} which game do you want ?', title='select game panel', buttons=['User - Bot', 'Bot - Bot', 'User - User', 'Rull game', 'Details', 'Exit'])
+    elif which_game == 'Exit' :
+        canceling = py.confirm(text='Are you sure to quit the game ?', title='Quit panel', buttons=['Yes', 'No'])
+        if canceling == 'Yes' :
+            canceling_2 = py.alert(text='Wish see you soon, Thanks for selecting us', title='Quit panel')
+            break
+        elif canceling == 'No' :
+            which_game = py.confirm(text=f'{username} which game do you want ?', title='select game panel', buttons=['User - Bot', 'Bot - Bot', 'User - User', 'Rull game', 'Details', 'Exit'])
 
 if which_game == 'User - Bot' :
-    bot_name = py.alert(text=f'Robot name is {random_bot_name}', title='User - Bot panel')
+    random_bot_name_1 = random.choice(all_bot_name)
+    round_number = int(py.prompt(text='How many round do you want to play ?', title='round panel', default='How many round'))
+    bot_name = py.alert(text=f'Robot name is {random_bot_name_1}', title='User - Bot panel')
     for round in range(1, round_number + 1) :
         random_action = random.choice(action)
         which_game = py.confirm(text=f'{username} which action do you want to select ?', title='select action panel', buttons=['Rock', 'Paper', 'Scissor'])
         if which_game == random_action :
-            user += 1
+            user_1 += 1
             bot_1 += 1
-            resualt_game = py.alert(text=f'round {round} is equal, {random_bot_name} selected {random_action}, Resualt : {user} - {bot_1}', title='User - Bot panel')
+            resualt_game = py.alert(text=f'round {round} is equal, {random_bot_name_1} selected {random_action}, Resualt : {user_1} - {bot_1}', title='User - Bot panel')
         elif ( which_game == 'Paper' and random_action == 'Rock' ) or ( which_game == 'Rock' and random_action == 'Scissor' ) or ( which_game == 'Scissor' and random_action == 'Paper' ) :
-            user += 1
-            resualt_game = py.alert(text=f'round {round} You win, {random_bot_name} selected {random_action}, Resualt : {user} - {bot_1}', title='User - Bot panel')
+            user_1 += 1
+            resualt_game = py.alert(text=f'round {round} You win, {random_bot_name_1} selected {random_action}, Resualt : {user_1} - {bot_1}', title='User - Bot panel')
         else :
             bot_1 += 1
-            resualt_game = py.alert(text=f'round {round} You lose, {random_bot_name} selected {random_action}, Resualt : {user} - {bot_1}', title='User - Bot panel')
+            resualt_game = py.alert(text=f'round {round} You lose, {random_bot_name_1} selected {random_action}, Resualt : {user_1} - {bot_1}', title='User - Bot panel')
 
 elif which_game == 'Bot - Bot' :
-    bot_name_1 = py.alert(text=f'First Robot name is {random_bot_name}', title='Bot - Bot panel')
-    bot_name_1 = random_bot_name
+    random_bot_name_1 = random.choice(all_bot_name)
+    random_bot_name_2 = random.choice(all_bot_name)
+    round_number = int(py.prompt(text='How many round are played ?', title='round panel', default='How round'))
+    bot_name_1 = py.alert(text=f'First Robot name is {random_bot_name_1}', title='Bot - Bot panel')
+    bot_name_1 = random_bot_name_1
     random_bot_name = random.choice(all_bot_name)
     while bot_name_1 == random_bot_name :
         random_bot_name = random.choice(all_bot_name)
@@ -79,6 +97,7 @@ elif which_game == 'Bot - Bot' :
             resualt_game = py.alert(text=f'Extra round ,resualt is equal : {bot_1} - {bot_2} Are you ready ?', title='Bot - Bot panel')
 
 elif which_game == 'User - User' :
+    round_number = int(py.prompt(text='How many round are played ?', title='round panel', default='How round'))
     username_1 = py.prompt(text=f'{username} enter the first name player please', title='Name panel', default='first player')
     username_2 = py.prompt(text='And enter the second name player', title='Name panel', default='second player')
 
@@ -90,23 +109,20 @@ elif which_game == 'User - User' :
         which_game_2 = py.confirm(text=f'{username_2} which action do you want to select ?', title='select action panel', buttons=['Rock', 'Paper', 'Scissor'])
 
         if which_game_1 == which_game_2 :
-            user += 1
+            user_1 += 1
             user_2 += 1
-            resualt_game = py.alert(text=f'round {round} is equal, Resualt : {username_1} {user} - {user_2} {username_2}', title='User - User panel')
+            resualt_game = py.alert(text=f'round {round} is equal, Resualt : {username_1} {user_1} - {user_2} {username_2}', title='User - User panel')
         elif ( which_game_1 == 'Paper' and which_game_2 == 'Rock' ) or ( which_game_1 == 'Rock' and which_game_2 == 'Scissor' ) or ( which_game_1 == 'Scissor' and which_game_2 == 'Paper' ) :
-            user += 1
-            resualt_game = py.alert(text=f'round {round} {username_1} is win, {username_1} selected {which_game_1}, Resualt : {user} - {user_2}', title='User - Bot panel')
+            user_1 += 1
+            resualt_game = py.alert(text=f'round {round} {username_1} is win, {username_1} selected {which_game_1}, Resualt : {user_1} - {user_2}', title='User - Bot panel')
         else :
             user_2 += 1
-            resualt_game = py.alert(text=f'round {round} {username_2} is win, {username_2} selected {which_game_2}, Resualt : {user} - {user_2}', title='User - Bot panel')
+            resualt_game = py.alert(text=f'round {round} {username_2} is win, {username_2} selected {which_game_2}, Resualt : {user_1} - {user_2}', title='User - Bot panel')
         
-elif which_game == 'Rull game' :
-    Rull_game = py.alert(text='This game produced by Milad Barouni and the Ways of communication by Email : milad.baroun@gmail.com and Phone number : +989177719601', title='Details panel')
-    which_game = py.confirm(text=f'{username} which game do you want ?', title='select game panel', buttons=['User - Bot', 'Bot - Bot', 'User - User', 'Rull game', 'Details'])
-    round_number = int(py.prompt(text='How many round are played ?', title='round panel', default='How round'))
 
-elif which_game == 'Details' :
-    pass
+
+
+
 
 
 
@@ -134,3 +150,26 @@ elif which_game == 'Details' :
 
 
 
+
+# while which_game == 'Rull game' :
+#     Rull_game = py.alert(text='Rull of the game: 1. Playing the game means you have accepted the complete rules of the game. 2. When your opponent is a robot, his movement is chosen by chance and you have to accept the result. If you make a change in the source code of the game, this is cheating. 3. During the game, two users should not show each other their next move to the opponent. 4. If you withdraw from the game, your opponent will win the game.', title='Rull panel')
+#     which_game = py.confirm(text=f'{username} which game do you want ?', title='select game panel', buttons=['User - Bot', 'Bot - Bot', 'User - User', 'Rull game', 'Details', 'Exit'])
+#     if which_game == 'Rull game' :
+#         Rull_game = py.alert(text='Rull of the game: 1. Playing the game means you have accepted the complete rules of the game. 2. When your opponent is a robot, his movement is chosen by chance and you have to accept the result. If you make a change in the source code of the game, this is cheating. 3. During the game, two users should not show each other their next move to the opponent. 4. If you withdraw from the game, your opponent will win the game.', title='Rull panel')
+
+
+# while which_game == 'Details' :
+#     Details_game = py.alert(text='This game produced by Milad Barouni and the Ways of communication by Email : milad.baroun@gmail.com and Phone number : +989177719601', title='Details panel')
+#     which_game = py.confirm(text=f'{username} which game do you want ?', title='select game panel', buttons=['User - Bot', 'Bot - Bot', 'User - User', 'Rull game', 'Details', 'Exit'])
+#     if which_game == 'Detail' :
+#         Rull_game = py.alert(text='This game produced by Milad Barouni and the Ways of communication by Email : milad.baroun@gmail.com and Phone number : +989177719601', title='Details panel')
+
+
+# while which_game == 'Exit' :
+#     canceling = py.confirm(text='Are you sure to quit the game ?', title='Quit panel', buttons=['Yes', 'No'])
+#     if canceling == 'Yes' :
+#         canceling_2 = py.alert(text='Wish see you soon, Thanks for selecting us', title='Quit panel')
+#         break
+#     while canceling == 'No' :
+#         which_game = py.confirm(text=f'{username} which game do you want ?', title='select game panel', buttons=['User - Bot', 'Bot - Bot', 'User - User', 'Rull game', 'Details', 'Exit'])
+#         break
